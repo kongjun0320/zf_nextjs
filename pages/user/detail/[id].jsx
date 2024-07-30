b75d2c87f322521401805cd14e0e047238f99cc6
aicherish0320 <aicherish0320@163.com>
2024-07-30 21:47:21 +0800

    feat: 其它方法

--- pages/user/detail/[id].jsx
+++ pages/user/detail/[id].jsx
@@ -16,9 +16,34 @@ function UserDetail(props) {
   );
 }
 
-UserDetail.getInitialProps = async ({ query }) => {
-  const response = await request.get(`/api/users/${query.id}`);
-  return { user: response.data.data };
-};
+// UserDetail.getInitialProps = async ({ query }) => {
+//   const response = await request.get(`/api/users/${query.id}`);
+//   return { user: response.data.data };
+// };
+
+// 此方法会在每次客户端请求服务器的时候调用
+export async function getServerSideProps() {
+  const response = await request.get(
+    `http://localhost:3333/api/users/${query.id}`
+  );
+  return {
+    props: {
+      user: response.data.data,
+    },
+  };
+}
+
+// 此函数仅会在编译阶段使用
+export async function getStaticPaths({ params }) {
+  const response = await request.get('http://localhost:3333/api/users');
+  const users = response.data.data;
+  // 这是一个用户详情页的路径的字符串数组
+  // 在编译的时候，会调用此方法，获取路径的数组，然后依次访问这些路径，把这个路径生成静态的 HTML 文件
+  const paths = users.map((user) => `/user/detail/${user.id}`);
+
+  return {
+    paths,
+  };
+}
 
 export default UserDetail;
--- pages/user/list.jsx
+++ pages/user/list.jsx
@@ -18,12 +18,32 @@ function UserList(props) {
 }
 
 // 在组件里定义好，然后在服务器端获取数据
-UserList.getInitialProps = async () => {
-  console.log('UserList getInitialProps');
-  const response = await request.get('/api/users');
+// UserList.getInitialProps = async () => {
+//   console.log('UserList getInitialProps');
+//   const response = await request.get('/api/users');
+//   return {
+//     list: response?.data?.data,
+//   };
+// };
+
+// 此方法会在每次客户端请求服务器的时候调用
+export async function getServerSideProps() {
+  const response = await request.get('http://localhost:3333/api/users');
   return {
-    list: response?.data?.data,
+    props: {
+      list: response?.data?.data,
+    },
   };
-};
+}
+
+// 此函数仅会在编译阶段使用
+export async function getStaticProps() {
+  const response = await request.get('http://localhost:3333/api/users');
+  return {
+    props: {
+      list: response?.data?.data,
+    },
+  };
+}
 
 export default UserList;
