f7dfbc5cf9941c57b689f4185f7c05a9f9758410
aicherish0320 <aicherish0320@163.com>
2024-07-30 08:01:18 +0800

    feat: 自定义 document

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <style>
+            {`
+                body {
+                    background: green;
+                }
+            `}
+          </style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Head>
+        <title>首页</title>
+        <meta name="description" content="内容" />
+      </Head>
+      <p>Home</p>
+    </div>
+  );
 }
 
 export default Home;
